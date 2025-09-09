a746fbd75d370eb6d0e373b98c88fed75103b0ad
aatif <aa@aatif.com>
2025-09-09 11:21:12 +0530

    9-9

---
+++ src/Collections/A1.java
@@ -0,0 +1,15 @@
+package Collections;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class A1 {
+    public static void main(String[] args) {
+        ArrayList list=new ArrayList();
+        list.add(10);
+        list.add("Shubham");
+        list.add(true);
+        list.add(12.5);
+        System.out.println(list);
+    }
+}
