d401d2a58259562e7586e9aac183f4702c53c4d9
aatif <aa@aatif.com>
2025-09-12 10:31:29 +0530

    New

--- src/Collections/A1.java
+++ src/Collections/A1.java
@@ -1,15 +1,17 @@
 package Collections;
 
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 public class A1 {
     public static void main(String[] args) {
         ArrayList list=new ArrayList();
-        list.add(10);
         list.add("Shubham");
         list.add(true);
         list.add(12.5);
-        System.out.println(list);
+
     }
 }
